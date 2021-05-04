6c84df4e76b6a5a4626186ce79579ce5d021c5e5
g770728y <g770728y@163.com>
2021-05-04 16:14:26 +0800

    settingsStore可关闭标签

--- src/baseLayouts/page/CrudPageLayout/index.tsx
+++ src/baseLayouts/page/CrudPageLayout/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import styles from './index.module.less';
+import { SmallDashOutlined } from '@ant-design/icons';
+import { useMouseDownMoveUp } from 'valor-hooks';
 
 interface Props {
   query?: React.ReactNode;
@@ -9,7 +11,53 @@ interface Props {
   style?: React.CSSProperties;
 }
 
+const rawHeight = 350;
 const CrudPageLayout: React.FC<Props> = ({ query, action, table, slave, style }) => {
+  const [state, setState] = React.useState<{
+    slaveHeight: number;
+    isMoving: boolean;
+    slaveHeight0: number;
+  }>({ slaveHeight: rawHeight, isMoving: false, slaveHeight0: rawHeight });
+  // 未使用
+  const startPointRef = React.useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const handleMouseMove = React.useCallback(
+    ({ dx, dy, x, y }: { dx: number; dy: number; x: number; y: number }) => {
+      setState(s => {
+        const nextSlaveHeight = Math.max(s.slaveHeight0 - dy, 200);
+        if (!s.isMoving) {
+          return {
+            ...s,
+            slaveHeight: nextSlaveHeight,
+            isMoving: true,
+            slaveHeight0: s.slaveHeight,
+          };
+        }
+        return { ...s, slaveHeight: nextSlaveHeight };
+      });
+    },
+    [],
+  );
+  const handleMouseUp = React.useCallback(
+    ({ dx, dy, x, y }: { dx: number; dy: number; x: number; y: number }) => {
+      setState(s => {
+        const nextSlaveHeight = Math.max(s.slaveHeight0 - dy, 200);
+        return {
+          ...s,
+          slaveHeight: nextSlaveHeight,
+          isMoving: false,
+          slaveHeight0: nextSlaveHeight,
+        };
+      });
+    },
+    [],
+  );
+
+  const { handleMouseDown } = useMouseDownMoveUp({
+    onMouseMove: handleMouseMove,
+    onMouseUp: handleMouseUp,
+    cursor: 'ns-resize',
+    startPointRef,
+  });
   const mainContent = (
     <>
       {query && <div className={styles['query']}>{query}</div>}
@@ -20,15 +68,12 @@ const CrudPageLayout: React.FC<Props> = ({ query, action, table, slave, style })
   return (
     <div className={styles['container']} style={style}>
       {slave ? (
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <div className={styles['master-slave-container']}>
           <div style={{ flex: 'auto', height: 10, overflowY: 'auto' }}>{mainContent}</div>
-          <div
-            style={{
-              flex: 'none',
-              height: 350,
-              borderTop: '5px solid #ccc' /*boxShadow: '0px 0px 5px #ddd'*/,
-            }}
-          >
+          <div className={styles['slave-container']} style={{ height: state.slaveHeight }}>
+            <div className={styles['handler']} onMouseDown={handleMouseDown}>
+              <SmallDashOutlined />
+            </div>
             {slave}
           </div>
         </div>
--- src/globalStores/SettingsStore.ts
+++ src/globalStores/SettingsStore.ts
@@ -43,6 +43,19 @@ export class SettingsStore {
       this.activeMenuKey = menuKey;
     }
   };
+
+  // 用于关闭标签tab
+  @action
+  unsetActiveMenu = (menuKey: string) => {
+    if (this.data.useTabs && this.tabKeys.length >= 2) {
+      const index = this.tabKeys.indexOf(menuKey);
+      if (index >= 0) {
+        const nextActiveIndex = index === this.tabKeys.length - 1 ? index - 1 : index;
+        this.tabKeys = this.tabKeys.filter(it => it !== menuKey);
+        this.activeMenuKey = this.tabKeys[nextActiveIndex];
+      }
+    }
+  };
 }
 
 // settingsStore应该在程序中实例化
